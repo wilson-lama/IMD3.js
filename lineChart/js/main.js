7c533223c81030782912c904aec2d74baf185113
wilson-lama <w.lamaa10@gmail.com>
2022-12-04 15:09:18 -0500

    use d3.tsv instead of promise syntax

--- lineChart/js/main.js
+++ lineChart/js/main.js
@@ -1,22 +1,9 @@
 let lineChart;
 
-// load data using promises
-let promises = [
-    d3.tsv("data/title.basics.tsv"),
-];
+loadData();
 
-Promise.all(promises)
-    .then(function (data) {
-        initMainPage(data);
-    })
-    .catch(function (err) {
-        console.log(err);
+function loadData() {
+    d3.tsv("data/title.basics.tsv").then(data => {
+        lineChart = new LineChart("lineChart", data);
     });
-
-function initMainPage(allDataArray) {
-    console.log(allDataArray);
-    lineChart = new LineChart(
-        "lineChart",
-        allDataArray[0],
-    );
 }
