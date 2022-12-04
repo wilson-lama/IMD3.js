21169cc84ef226488de2d031f5f51d2d008bf7f3
tomzhang255 <tomzhang255@gmail.com>
2022-12-03 19:00:03 -0500

    highlight leo in bubble chart

--- beeswarmChart/js/beeswarmChart.js
+++ beeswarmChart/js/beeswarmChart.js
@@ -80,6 +80,7 @@ function BubbleChart(
         .selectAll("a")
         .data(root.leaves())
         .join("a")
+        .attr("id", (d) => d.data)
         .attr(
             "xlink:href",
             link == null ? null : (d, i) => link(D[d.data], i, data)
@@ -106,11 +107,15 @@ function BubbleChart(
             .style("top", event.pageY + "px").html(`
                 <div style="border: thin solid grey; border-radius: 5px; background: darkgrey; padding: 5px">
                     <h6 class="text-center">${
-                        this.querySelector("title").innerHTML
+                        // this.querySelector("title").innerHTML
+                        data[this.id].movie
                     }</h6>
-                    <span class="text-center">Revenue: ${d3.format("$,")(
-                        d.value
-                    )}</span>
+                    <div>Revenue: ${
+                        // d3.format("$,.2r")(4223)
+                        data[this.id].revenue
+                    }</div>
+                    <div>Rank: ${data[this.id].rank} / 200</div>
+                    <div>Year: ${data[this.id].year}</div>
                 </div>`);
     }).on("mouseout", function (event, d) {
         tooltip.style("opacity", 0);
@@ -161,6 +166,8 @@ class BeeswarmChart {
             label: (d) => d.movie,
             value: (d) => Number(d.revenue.slice(1).split(",").join("")),
             title: (d) => d.movie,
+            group: (d) => d.leo,
+            colors: ["lightgrey", "orange"],
             width: 1152,
         });
 
@@ -179,79 +186,6 @@ class BeeswarmChart {
 
         // d3.select("#" + vis.parentElement).append(vis.chart)
         document.querySelector("#" + vis.parentElement).append(vis.chart);
-
-        // SVG drawing area
-        vis.svg = d3
-            .select("#" + vis.parentElement)
-            .append("svg")
-            .attr("width", vis.width + vis.margin.left + vis.margin.right)
-            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-            .append("g")
-            .attr(
-                "transform",
-                "translate(" + vis.margin.left + "," + vis.margin.top + ")"
-            );
-
-        // Scales and axes
-        vis.x = d3.scaleLog().rangeRound([0, vis.width]);
-
-        let revenueMin = d3.min(vis.data, (d) =>
-            Number(d.revenue.slice(1).split(",").join(""))
-        );
-        let revenueMax = d3.max(vis.data, (d) =>
-            Number(d.revenue.slice(1).split(",").join(""))
-        );
-        let revenueScale = d3
-            .scaleLinear()
-            .domain([revenueMin, revenueMax])
-            .range([5, 30]);
-
-        vis.tooltip = d3
-            .select("body")
-            .append("div")
-            .attr("class", "tooltip")
-            .attr("id", "pieTooltip");
-
-        let circles = vis.svg
-            .selectAll("circle")
-            .data(vis.data)
-            .enter()
-            .append("circle")
-            .attr("r", (row_data, row_number) => {
-                return revenueScale(
-                    Number(row_data.revenue.slice(1).split(",").join(""))
-                );
-            })
-            .attr("cx", (row_data, row_number) => {
-                return ((row_data.rank - 1) % 10) * 100;
-            })
-            .attr("cy", (row_data, row_number) => {
-                return 50 * (row_data.rank / 10);
-            })
-            .attr("fill", "royalblue")
-            .on("mouseover", function (event, d) {
-                d3.select(this)
-                    .attr("stroke-width", "2px")
-                    .attr("stroke", "black")
-                    .attr("fill", "rgba(173,222,255,0.62)");
-                vis.tooltip
-                    .style("opacity", 1)
-                    .style("left", event.pageX + 20 + "px")
-                    .style("top", event.pageY + "px").html(`
-         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
-             <h3>Ranking #${d.rank}<h3>
-             <h4> Movie: ${d.movie}</h4>      
-             <h4> Year: ${d.year}</h4> 
-             <h4> Revenue: ${d.revenue}</h4>                          
-         </div>`);
-            });
-
-        vis.svg
-            .append("g")
-            .attr("class", "x-axis axis")
-            .attr("transform", "translate(0," + vis.height + ")");
-
-        vis.wrangleData();
     }
 
     wrangleData() {
